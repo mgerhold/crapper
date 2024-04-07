b6c48987d5e5dfe0646db2f6647d5f4e5eaf7c79
Michael Gerhold <michael.gerhold@web.de>
2024-04-07 21:00:25 +0200

    fix example

--- examples/simple-get-request/main.cpp
+++ examples/simple-get-request/main.cpp
@@ -5,10 +5,5 @@ int main() {
     static constexpr auto url = "https://official-joke-api.appspot.com/random_joke";
 
     auto const response = Crapper{}.get(url).send();
-
-    std::cout << "num bytes received: " << response.length() << '\n';
-    std::cout << response << '\n';
-
-    auto crapper = Crapper{};
-    std::cout << crapper.get(url).send() << '\n';
+    std::cout << response.body() << '\n';
 }
